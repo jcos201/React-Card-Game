5a1013572844bc9af296663667c8a43c14981978
Jorge L. Cossi <jcos201@gmail.com>
2021-08-04 13:31:57 -0400

    adds player hand calculation functionality

--- src/services/blackjackService.js
+++ src/services/blackjackService.js
@@ -8,7 +8,8 @@ function dealBlackJack() {
         ).then(response => {
             if(response.ok) return response.json();
             throw new Error('Well this is embarassing, something went wrong ');
-        }).then(data => setDeckID(data.deck_id))
+        }).then(data => setDeckID(data.deck_id));
+
 }
 
 function shuffleBlackJackDeck() {
@@ -20,7 +21,45 @@ function shuffleBlackJackDeck() {
     }).then(data => console.log(data))
 }
 
+function drawCards(num) {
+    let id = getDeckID();
+    return fetch(BASE_URL+id+'/draw/?count='+num
+    ).then(response=>{
+        if(response.ok) return response.json();
+        throw new Error(`The dealer is missing, can't deal cards right now.`)
+    }).then(data => {
+        let arr = [];
+        data.cards.map(card => {
+            arr.push([card])
+        })
+        return arr;
+    })
+}
+
+function calculateHand(cards) {
+    if(cards){
+        const reducer = (currentValue, card) => {
+            let val = card.value;
+            if(val === "KING" || val === "JACK" || val === "QUEEN"){
+                return currentValue += 10;
+            } else if(val !== "ACE") {
+                let num = parseInt(val);
+
+                return currentValue += num;
+            } else if(val < 12) {
+                return currentValue += 10;
+            } else {
+                return currentValue += 1;
+            }
+        }
+
+        return cards.reduce(reducer, 0);
+    } else return 0;
+}
+
 export {
     dealBlackJack,
     shuffleBlackJackDeck,
+    drawCards,
+    calculateHand,
 }

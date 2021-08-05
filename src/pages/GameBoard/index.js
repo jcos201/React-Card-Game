d2868dae2b45ebf0b077bb889690aedbb3ffbe85
Jorge L. Cossi <jcos201@gmail.com>
2021-08-05 06:42:22 -0400

    Adds Poker game functionality

--- src/pages/GameBoard/index.js
+++ src/pages/GameBoard/index.js
@@ -1,18 +1,22 @@
 import styled from "styled-components";
-import { useState} from "react";
+import { useState, useEffect } from "react";
 
-import { dealBlackJack, shuffleBlackJackDeck } from "../../services/blackjackService";
+import Card from "./Card";
+
+import { dealBlackJack, shuffleBlackJackDeck, drawCards, calculateHand } from "../../services/blackjackService";
 
 const Table = styled.section `
 display: flex;
 flex-direction: column;
 justify-content: space-around;
 align-items: space-around;
-min-height: 80vh;
+min-height: 75vh;
 min-width: 90vw;
 border: 18px solid brown;
 background-color: green;
 border-radius: 0 0 170px 170px;
+box-shadow: inset 0 0 10px #000000;
+margin-bottom: 10px;
 `
 
 const UserInputs = styled.section `
@@ -21,10 +25,9 @@ justify-content:space-around;
 `
 
 const Cardholder = styled.div `
-border: 5px dashed white;
+display: flex;
 min-height: 10vh;
 min-width: 10vw;
-width: 10vw;
 margin: 0 10px;
 `
 
@@ -34,55 +37,189 @@ justify-content: space-around;
 `
 
 const DeckButton = styled.button `
-border: 1px solid white;
-border-radius: 50%;
+height: 25px;
+line-height: 20px;
+width: 55px;
+border: 1px solid orange;
+border-radius: 5%;
 `
 
-function GameBoard(props) {
-    const [cardsDealt, setCardsDealt] = useState(false)
 
+
+const ScoreRow = styled.section`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+const Score = styled.div `
+height: 30px;
+background-color: white;
+color: black;
+text-align: center;
+box-shadow: inset 0 0 10px #000000;
+line-height: 30px;
+width: 20%;
+`
+
+
+function GameBoard(props) {
+    const [cardsDealt, setCardsDealt] = useState(false);
+
+    const [playerCards, setPlayerCards] = useState([]);
+    const [playerHand, setPlayerHand] = useState(0);
+    const [dealerCards, setDealerCards]  = useState([]);
+    const [dealerHand, setDealerHand] = useState(0);
+
+    const [showDealerCards, setShowDealerCards] = useState(false);
+
+    /*
+    handleDeal function is only used at the beginning of a new hand
+    */
     async function handleDeal(event) {
         event.preventDefault();
         try {
             await dealBlackJack();
-            setCardsDealt(true)
+            let cards = await drawCards(4);
+
+            let player = [cards[0][0],cards[2][0]]
+            let dealer = [cards[1][0],cards[3][0]]
+
+            setPlayerCards(prevState => ([
+                ...prevState,
+                ...player,
+            ]));
+
+            setDealerCards(prevState => ([
+                ...prevState,
+                ...dealer,
+            ]))
+
+            setCardsDealt(true);
+        } catch (error) {
+            alert(error.message)
+        }
+    };
+
+    async function handleHit(event){
+        event.preventDefault();
+        try{
+            let card = await drawCards(1);
+
+            setPlayerCards(prevState => ([
+                ...prevState,
+                card[0][0],
+            ]));
+        } catch (error) {
+            alert(error.message)
+        }
+        
+    }
+    
+    async function handleShuflle(event) {
+        event.preventDefault();
+        try {
+            await shuffleBlackJackDeck();
+            setPlayerCards([]);
+            setDealerCards([]);
+            setShowDealerCards(false);
+            setCardsDealt(false);
+            handleDeal(event);
         } catch (error) {
             alert(error.message)
         }
     }
 
-    async function handleShuflle(event) {
+    function playerStay(event) {
         event.preventDefault();
-        try {
-            await shuffleBlackJackDeck();
-        } catch (error) {
-            alert(error.message)
-        }
+        setShowDealerCards(true);
     }
+
+    useEffect(() => {
+        setPlayerHand(calculateHand(playerCards));
+        if(showDealerCards) {
+            setDealerHand(calculateHand(dealerCards));
+        } else if(cardsDealt){
+            setDealerHand(calculateHand([dealerCards[0]]));
+        }
+    }, [playerCards, dealerCards,showDealerCards,cardsDealt])
 
     return(<div>
 
 
         <Table>
             <Cardrow>
-                <Cardholder></Cardholder>
-            </Cardrow>
-            <section>
+                <Cardholder>
 
-            <p>Blackjack 21!</p>
-            </section>
-        {cardsDealt ? <p>Cards have been dealt</p> : <p>cards have not been dealt</p>}
+
+                {dealerCards && dealerCards.length > 0 ?
+                dealerCards.map((card, idx) => {
+                    return(<Card 
+                        {...props}
+                        value={card.value}
+                        image={card.image}
+                        showCard={idx === 1 ? showDealerCards : true}
+                        key={idx}
+                        />)
+                })
+                :
+                <>
+                    <p></p>
+                </>
+
+                }
+                </Cardholder>
+            </Cardrow>
+            <ScoreRow>
+            <div>
+            {showDealerCards ? 
+                    "Dealer Score:"
+                    :
+                    "Dealer is showing:"
+                }
+            </div>
+            <Score>
+                
+                {dealerHand}
+            </Score>
+                <div>
+                {cardsDealt ?
+                    <p>Your Score:</p>
+                    :
+                    <p>Click on 'Deal' to start game</p>
+                }
+                </div>
+            <Score>
+                
+                {playerHand}
+            </Score>
+            </ScoreRow>
             <Cardrow>
-            <Cardholder/>
-            <Cardholder/>
-            <Cardholder/>
+            <Cardholder>
+                {playerCards && playerCards.length > 0 ?
+                playerCards.map((card, idx) => {
+                    return(<Card 
+                        {...props}
+                        value={card.value}
+                        image={card.image}
+                        showCard={true}
+                        key={idx}
+                        />)
+                })
+                :
+                <p>No cards drawn yet</p>
+
+                }
+            </Cardholder>
             </Cardrow>
         </Table>
         <UserInputs>
-
-        {cardsDealt ? 
-            <DeckButton onClick={handleShuflle}>Restart</DeckButton> : 
-            <DeckButton onClick={handleDeal}>Deal</DeckButton> }
+            <DeckButton onClick={handleHit}>Hit</DeckButton>
+            <DeckButton onClick={playerStay}>Stay</DeckButton>
+            {cardsDealt ? 
+                <DeckButton onClick={handleShuflle}>Restart</DeckButton> : 
+                <DeckButton onClick={handleDeal}>Deal</DeckButton> }
         </UserInputs>
             
             
